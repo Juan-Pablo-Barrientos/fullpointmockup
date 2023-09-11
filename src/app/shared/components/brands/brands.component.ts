8fa19d5d89fe872fe4c015ba803a4154a9fb4bdf
Juan Pablo Barrientos <jpbarrientosros@gmail.com>
2023-09-11 12:41:28 -0300

    infinite sushi y representacion solapa fixed

--- src/app/shared/components/brands/brands.component.ts
+++ src/app/shared/components/brands/brands.component.ts
@@ -1,6 +1,7 @@
 import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit, HostListener, ViewChild } from '@angular/core';
 import { NgbCarousel, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Subscription, timer } from 'rxjs';
 
 @Component({
   selector: 'app-brands',
@@ -9,48 +10,7 @@ import { NgbCarousel, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 })
 export class BrandsComponent implements OnInit {
   slideIn = false;
-
-  constructor() { }
-  ngOnInit(): void {
-    this.rotateSushiBelt();
-  }
-  rotateSushiBelt(): void {
-    const sushiBelt = document.querySelector('.sushi-belt') as HTMLElement;
-    let currentPosition = 400;
-    const plateWidth = 250; // Adjust the plate width according to your design
-    let lastTimestamp = 0;
-
-    const animate = (timestamp: number) => {
-      if (!lastTimestamp) {
-        lastTimestamp = timestamp;
-      }
-      const deltaTime = timestamp - lastTimestamp;
-      lastTimestamp = timestamp;
-
-      currentPosition -= (plateWidth * deltaTime) / 2000; // Calculate the position change per second
-
-      sushiBelt.style.transform = `translateX(${currentPosition}px`;
-
-      // Check if the belt has looped
-      if (currentPosition < -plateWidth * (this.brands.length - 3)) {
-        currentPosition = 400;
-      }
-
-      requestAnimationFrame(animate);
-    };
-
-    requestAnimationFrame(animate);
-  }
-
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    const yOffset = window.scrollY;
-    // Adjust the condition based on when you want the slide effect to occur
-    if (yOffset > 200) {
-      this.slideIn = true;
-    }
-  }
-
+  subscribedTimer!:Subscription;
   brands:{image:string}[]=[
     {
       image:'../../../../assets/logos/Pritty.png'
@@ -88,5 +48,130 @@ export class BrandsComponent implements OnInit {
     {
       image:'../../../../assets/logos/marolio.png'
     },
+    {
+      image:'../../../../assets/logos/LOGO_BRANCA_FERNET.png'
+    },
+    {
+      image:'../../../../assets/logos/NORTON-LOGO.jpeg'
+    },
+    {
+      image:'../../../../assets/logos/Pernod-Ricard-Simbolo.png'
+    },
+    {
+      image:'../../../../assets/logos/campari_logo_0_1_0.png'
+    },
+    {
+      image:'../../../../assets/bodegaLaRural.jpg'
+    },
+    {
+      image:'../../../../assets/logos/Energizer-logo.png'
+    },
+    {
+      image:'../../../../assets/logos/speed.jpg'
+    },
+    {
+      image:'../../../../assets/logos/pascual-toso.jpeg'
+    },
+    {
+      image:'../../../../assets/logos/marolio.png'
+    },
+    {
+      image:'../../../../assets/logos/LOGO_BRANCA_FERNET.png'
+    },
+    {
+      image:'../../../../assets/logos/NORTON-LOGO.jpeg'
+    },
+    {
+      image:'../../../../assets/logos/Pernod-Ricard-Simbolo.png'
+    },
+    {
+      image:'../../../../assets/logos/campari_logo_0_1_0.png'
+    },
+    {
+      image:'../../../../assets/bodegaLaRural.jpg'
+    },
+    {
+      image:'../../../../assets/logos/Energizer-logo.png'
+    },
+    {
+      image:'../../../../assets/logos/speed.jpg'
+    },
+    {
+      image:'../../../../assets/logos/pascual-toso.jpeg'
+    },
+    {
+      image:'../../../../assets/logos/marolio.png'
+    },
+    {
+      image:'../../../../assets/logos/LOGO_BRANCA_FERNET.png'
+    },
+    {
+      image:'../../../../assets/logos/NORTON-LOGO.jpeg'
+    },
+    {
+      image:'../../../../assets/logos/Pernod-Ricard-Simbolo.png'
+    },
+    {
+      image:'../../../../assets/logos/campari_logo_0_1_0.png'
+    },
+    {
+      image:'../../../../assets/bodegaLaRural.jpg'
+    },
+    {
+      image:'../../../../assets/logos/Energizer-logo.png'
+    },
+    {
+      image:'../../../../assets/logos/speed.jpg'
+    },
+    {
+      image:'../../../../assets/logos/pascual-toso.jpeg'
+    },
+    {
+      image:'../../../../assets/logos/marolio.png'
+    },
   ]
+
+  constructor() { }
+  ngOnInit(): void {
+    this.rotateSushiBelt();
+  }
+
+  rotateSushiBelt(): void {
+    const sushiBelt = document.querySelector('.sushi-belt') as HTMLElement;
+    let currentPosition = 0;
+    const plateWidth = 250; // Adjust the plate width according to your design
+    let lastTimestamp = 0;
+
+    const animate = (timestamp: number) => {
+      if (!lastTimestamp) {
+        lastTimestamp = timestamp;
+      }
+      const deltaTime = timestamp - lastTimestamp;
+      lastTimestamp = timestamp;
+
+      currentPosition -= (plateWidth * deltaTime) / 2000; // Calculate the position change per second
+
+      sushiBelt.style.transform = `translateX(${currentPosition}px`;
+
+      // Check if the belt has looped
+      if (currentPosition < -plateWidth * (this.brands.length - 10)) {
+        currentPosition = 0;
+      }
+
+      requestAnimationFrame(animate);
+    };
+
+    requestAnimationFrame(animate);
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const yOffset = window.scrollY;
+    // Adjust the condition based on when you want the slide effect to occur
+    if (yOffset > 200) {
+      this.slideIn = true;
+    }
+  }
+
+
 }
