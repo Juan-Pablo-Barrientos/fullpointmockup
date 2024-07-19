e80bd6b752ec938db9da24f0fc13172431b607bc
Juan-Pablo-Barrientos <jpbarrientosros@outlook.com>
2024-07-19 10:19:09 -0300

    fix

--- src/app/shared/components/sendCV/sendCV.component.ts
+++ src/app/shared/components/sendCV/sendCV.component.ts
@@ -65,10 +65,12 @@ export class SendCVComponent implements OnInit {
   }
 
   sendEmail() {
+    this.sendCVDisabled = true;
     this.getFormValidationErrors();
     if (this.errors.length !== 0 || !this.selectedFile) {
       this.toastr.error('Falta completar campos o los ha insertado mal', 'Error al enviar');
       this.sendCvForm.markAllAsTouched();
+      this.sendCVDisabled = false;
       return;
     }
 
